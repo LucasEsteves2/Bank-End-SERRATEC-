1bd282153acd65c58f76420dec106984f3b9ccc5
natalino <natalinoesteves2010@gmail.com>
2021-10-13 15:39:41 -0300

    safsadasdsad

--- BankEnd/src/br/com/residencia/bankend/principal/SistemaInterno.java
+++ BankEnd/src/br/com/residencia/bankend/principal/SistemaInterno.java
@@ -8,7 +8,8 @@ import br.com.residencia.telas.Transferencia;
 public class SistemaInterno {
 
 	public static void main(String[] args) {
-			//aguarde o processo
+		//aguarde o processo
+		//aguarde o processo
 		//machado98
 		//hohohohohoohho
 		Connection conexao = new Conexao().conectar();
