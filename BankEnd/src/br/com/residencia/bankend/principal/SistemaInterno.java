249544e2d400080b8f72885a3a03674b832820e1
natalino <natalinoesteves2010@gmail.com>
2021-10-13 14:42:18 -0300

    hohoh

--- BankEnd/src/br/com/residencia/bankend/principal/SistemaInterno.java
+++ BankEnd/src/br/com/residencia/bankend/principal/SistemaInterno.java
@@ -10,9 +10,7 @@ public class SistemaInterno {
 	public static void main(String[] args) {
 
 		//machado98
-		//aabcvsdsd
-		//machado98
-		//machado98
+		//hohohohohoohho
 		Connection conexao = new Conexao().conectar();
 
 		Transferencia tela1 = new Transferencia(conexao);
