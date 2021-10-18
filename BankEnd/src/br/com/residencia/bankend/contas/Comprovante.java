9e28546a7749af1801e037cd1a43c3e49a4e2ee2
natalino <natalinoesteves2010@gmail.com>
2021-10-18 20:32:54 -0300

    att hora comprovante

--- BankEnd/src/br/com/residencia/bankend/contas/Comprovante.java
+++ BankEnd/src/br/com/residencia/bankend/contas/Comprovante.java
@@ -12,8 +12,8 @@ public class Comprovante {
 	public static void deposito (Contas conta,Double valor,String tipoConta) throws IOException {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		String data = dtf.format(LocalDateTime.now());
+		
 		File arquivo = new File( "C:\\temp\\"+data+"  deposito.txt" );
-		boolean existe = arquivo.exists();
 		arquivo.createNewFile();
 		
 		FileWriter fw = new FileWriter( arquivo );
@@ -31,8 +31,6 @@ public class Comprovante {
 		bw.newLine();
 		bw.write( "valor do depósito : "+"R$ "+valor );
 		bw.newLine();
-		bw.close();
-		fw.close();
 		}
 		
 		else {
@@ -48,19 +46,18 @@ public class Comprovante {
 			bw.newLine();
 			bw.write( "valor do depósito : "+"R$ "+valor );
 			bw.newLine();
-			bw.close();
-			fw.close();
+		
 					
 		}
-		
+		bw.close();
+		fw.close();
 	}
 	
 	public static void saque (Contas conta,Double valor) throws IOException {
 	
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd HH.mm.ss");
 		String data = dtf.format(LocalDateTime.now());
 		File arquivo = new File( "C:\\temp\\"+data+"  saque.txt" );
-		boolean existe = arquivo.exists();
 		arquivo.createNewFile();
 		
 		FileWriter fw = new FileWriter( arquivo );
@@ -74,10 +71,9 @@ public class Comprovante {
 		bw.write( "valor do saque : "+"R$ "+valor );
 		bw.newLine();
 		bw.write( "Saldo  atual:  "+conta.getSaldo() );
-		bw.close();
-		fw.close();
 		
-		}
+		
+		}else {
 		bw.write( "--------------- Saque --------------- ");
 		bw.newLine();
 		bw.write( "Operação realizada : "+ "Saque" );
@@ -87,9 +83,10 @@ public class Comprovante {
 		bw.write( "valor do saque : "+"R$ "+valor );
 		bw.newLine();
 		bw.write( "Saldo  atual:  "+"R$ "+conta.getSaldo() );
+		
+		}
 		bw.close();
 		fw.close();
-		
 	}
 	
 	
@@ -97,7 +94,6 @@ public class Comprovante {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		String data = dtf.format(LocalDateTime.now());
 		File arquivo = new File( "C:\\temp\\"+data+"  Transferência.txt" );
-		boolean existe = arquivo.exists();
 		arquivo.createNewFile();
 		
 		FileWriter fw = new FileWriter( arquivo );
@@ -112,8 +108,7 @@ public class Comprovante {
 		bw.write( "favorecido  "+conta.getCliente().getNome() );
 		bw.newLine();
 		bw.write( "conta favorecida :  "+conta.getNumero() );
-		bw.newLine();
-		fw.close();
+		
 		}else {
 			bw.write( "--------------- Transferência --------------- ");
 			bw.newLine();
@@ -126,9 +121,10 @@ public class Comprovante {
 			bw.write( "favorecido  "+conta.getCliente().getNome() );
 			bw.newLine();
 			bw.write( "conta favorecida :  "+conta.getNumero() );
-			bw.close();
-			fw.close();
+			
 			}
+		bw.close();
+		fw.close();
 		}
 		
 	}
